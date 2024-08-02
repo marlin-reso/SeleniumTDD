5618a0a0d7da1b17f9d694dc385df97c27e22dfb
MARLIN <sanju.reso@gmail.com>
2024-08-02 21:47:15 +0530

    Java Script Executor updated

--- src/main/java/com/opencart/utils/JavaScriptUtil.java
+++ src/main/java/com/opencart/utils/JavaScriptUtil.java
@@ -1,5 +1,44 @@
 package com.opencart.utils;
 
-public class JavaScriptUtil {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class JavaScriptUtil {
+	
+	WebDriver driver;
+	
+	public JavaScriptUtil(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void flash(WebElement element) {
+		
+		String bgcolor = element.getCssValue("backgroundColor");//red
+		for(int i=0; i<10;i++) {
+			changeColor("rgb(0,200,0)", element);//green
+			changeColor(bgcolor, element);//red
+		}
+		
+		
+	}
+	
+	private void changeColor(String color, WebElement element) {
+		
+		JavascriptExecutor js = ((JavascriptExecutor) driver);
+		js.executeScript("arguments[0].style.backgroundColor = '" + color + "'", driver);
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
